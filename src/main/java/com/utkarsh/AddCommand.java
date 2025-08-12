712568de9356694f921775b31d422df2ee1226cc
utkarsh <utkarsh8377@gmail.com>
2025-08-13 01:59:59 +0530

    Addcommand.java banaya

--- src/main/java/com/utkarsh/AddCommand.java
+++ src/main/java/com/utkarsh/AddCommand.java
@@ -1,5 +1,18 @@
 package com.utkarsh;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Parameters;
 
-public class AddCommand {
+@Command(name = "add", description = "Add a new task to the list.")
+
+public class AddCommand implements Runnable{
+  @Parameters
+  (index="0",description = "The Description of the new task")
+  private String description;
+  @Override                
+  public void run(){
+    System.out.println("Adding a new task:"+ description );
+
   
+  }
+
 }
