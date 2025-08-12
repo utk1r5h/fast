0b3304a198302422479bbe3513201d35f60ab7a4
utkarsh <utkarsh8377@gmail.com>
2025-08-13 01:42:09 +0530

    App.java modified

--- src/main/java/com/utkarsh/App.java
+++ src/main/java/com/utkarsh/App.java
@@ -1,10 +1,25 @@
 package com.utkarsh;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
 
-/**
- * Hello world!
- */
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+@Command(name= "tm",
+mixinStandardHelpOptions = true,
+description="A fast CLI Task Manager",
+subcommands={
+    AddCommand.class
+
+    
+})
+
+public class App implements Runnable{
+    @Override
+    public void run(){
+        // if user just types tm with no commands
+        System.out.println("Please specify a command: add, list, etc. Use --help for more info.");
+
+    }
+    public static void main(String[] args){
+        int exitcode= new CommandLine(new App()).execute(args);
+        System.exit(exitcode);
     }
 }
