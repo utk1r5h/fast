143db4bad1cc88c3896f4b04e53d180512a57531
Samman <sammannsingh.031005@gmail.com>
2025-11-18 06:29:08 +0530

    registered all new commands in main application

--- src/main/java/com/utkarsh/App.java
+++ src/main/java/com/utkarsh/App.java
@@ -1,7 +1,6 @@
 package com.utkarsh;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
-
 @Command(name= "tm",
 mixinStandardHelpOptions = true,
 description="A fast CLI Task Manager",
@@ -10,17 +9,20 @@ subcommands={
     ListCommand.class,
     DoneCommand.class,
     ClearCommand.class,
-    EditCommand.class
-
-    
+    EditCommand.class,
+    SearchCommand.class,
+    StatsCommand.class,
+    TagsCommand.class,
+    UserCommand.class,
+    WatchCommand.class,
+    NotifyCommand.class,
+    ProcessCommand.class,
+    ExportCommand.class
 })
-
 public class App implements Runnable{
     @Override
     public void run(){
-        // if user just types tm with no commands
         System.out.println("Please specify a command: add, list, etc. Use --help for more info.");
-
     }
     public static void main(String[] args){
         int exitcode= new CommandLine(new App()).execute(args);
